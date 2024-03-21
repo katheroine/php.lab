6aca535669afc7fd3d51258252ce735b217c2c82
katheroine <katheroine@gmail.com>
2024-03-21 14:43:33 +0100

    Static variables (in classes) [#literals_constants_variables]

--- literals_constants_variables/static_variables.php
+++ literals_constants_variables/static_variables.php
@@ -17,3 +17,45 @@ someFunction();
 someFunction();
 
 print(PHP_EOL);
+
+class SomeClass
+{
+    public static $number;
+
+    static function someClassFunction()
+    {
+        printf("Number: %d\n", self::$number);
+
+        self::$number++;
+    }
+
+    function someObjectFunction()
+    {
+        printf("Number: %d\n", self::$number);
+
+        self::$number++;
+    }
+}
+
+SomeClass::someClassFunction();
+
+$someObject = new SomeClass();
+$someObject->someObjectFunction();
+
+$anotherObject = new SomeClass();
+$anotherObject->someObjectFunction();
+
+print(PHP_EOL);
+
+class SomeSubclass extends SomeClass
+{
+}
+
+SomeSubclass::someClassFunction();
+
+$someSubobject = new SomeSubclass();
+$someSubobject->someObjectFunction();
+
+$anotherSubobject = new SomeSubclass();
+$anotherSubobject->someObjectFunction();
+
