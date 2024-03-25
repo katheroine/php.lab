b937c75e68d4339973306b2db295b6162d18a9cf
katheroine <katheroine@gmail.com>
2024-03-25 12:36:58 +0100

    Inheritance example completed [#classes]

--- classes/inheritance.php
+++ classes/inheritance.php
@@ -51,22 +51,64 @@ class SomeClass
     }
 }
 
+class SomeSubclass extends SomeClass
+{
+    public const SOME_SUBCLASS_CONSTANT = 'nanana';
+    public static int $someSubclassVariable = 2048;
+    public string $someSubobjectVariable = 'hi';
+    public readonly float $anotherUnchengeableVariable;
+
+    public function __construct()
+    {
+        $this->anotherUnchengeableVariable = 2.5;
+    }
+
+    public static function someSubclassFunction(): void
+    {
+        parent::someClassFunction();
+        print(
+            SomeSubclass::SOME_SUBCLASS_CONSTANT . PHP_EOL
+            . self::SOME_SUBCLASS_CONSTANT . PHP_EOL
+            . self::$someSubclassVariable . PHP_EOL
+            . PHP_EOL
+        );
+    }
+
+    public function someSubobjectFunction(): void
+    {
+        parent::someClassFunction();
+        print(
+            SomeSubclass::SOME_CLASS_CONSTANT . PHP_EOL
+            . self::SOME_CLASS_CONSTANT . PHP_EOL
+            . self::$someSubclassVariable . PHP_EOL
+            . $this->someSubobjectVariable . PHP_EOL
+            . $this->anotherUnchengeableVariable . PHP_EOL
+            . PHP_EOL
+        );
+    }
+}
+
+SomeSubclass::someSubclassFunction();
+
+$someSubobject1 = new SomeSubclass();
+$someSubobject1->someSubobjectFunction();
+
 class SomePropertiesOverridingSubclass extends SomeClass
 {
-    public const SOME_CLASS_CONSTANT = 'ulala';
-    public static int $someClassVariable = 2048;
+    public const SOME_CLASS_CONSTANT = 'hahaha';
+    public static int $someClassVariable = 4096;
     public string $someObjectVariable = 'hey';
 }
 
 SomePropertiesOverridingSubclass::someClassFunction();
 
-$someSubobject1 = new SomePropertiesOverridingSubclass();
-$someSubobject1->someObjectFunction();
+$someSubobject2 = new SomePropertiesOverridingSubclass();
+$someSubobject2->someObjectFunction();
 
 class SomeOverridingSubclass extends SomeClass
 {
-    public const SOME_CLASS_CONSTANT = 'nanana';
-    public static int $someClassVariable = 4096;
+    public const SOME_CLASS_CONSTANT = 'hehehe';
+    public static int $someClassVariable = 8192;
     public string $someObjectVariable = 'welcome';
 
     public function __construct()
@@ -120,5 +162,5 @@ class SomeOverridingSubclass extends SomeClass
 
 SomeOverridingSubclass::someClassFunction();
 
-$someSubobject2 = new SomeOverridingSubclass();
-$someSubobject2->someObjectFunction();
+$someSubobject3 = new SomeOverridingSubclass();
+$someSubobject3->someObjectFunction();
