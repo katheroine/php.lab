9a90982016a152e0678500e6c9c752a6b372457b
katheroine <katheroine@gmail.com>
2024-05-25 01:45:09 +0200

    Exceptions: Value error [#library]

--- README.md
+++ README.md
@@ -128,6 +128,7 @@ Laboratory of PHP.
             1. [Error](./library/classes/exceptions/error.php)
             1. [Error exception](./library/classes/exceptions/error_exception.php)
             1. [Type error](./library/classes/exceptions/type_error.php)
+            1. [Value error](./library/classes/exceptions/value_error.php)
             1. [Unexpected value exception (SPL)](./library/classes/exceptions/spl_unexpected_value_exception.php)
             2. [Bad function call exception (SPL)](./library/classes/exceptions/spl_bad_function_call_exception.php)
             3. [Bad method call exception (SPL)](./library/classes/exceptions/spl_bad_method_call_exception.php)
---
+++ library/classes/exceptions/value_error.php
@@ -0,0 +1,21 @@
+<?php
+
+// "A ValueError is thrown when the type of an argument is correct but the value of it is incorrect.
+// For example, passing a negative integer when the function expects a positive one,
+// or passing an empty string/array when the function expects it to not be empty."
+// -- https://www.php.net/manual/en/class.valueerror.php
+
+try {
+    json_decode('{}', true, -1);
+} catch (ValueError $exception) {
+    print('As string: ' . $exception . PHP_EOL . PHP_EOL);
+    print('Message: ' . $exception->getMessage() . PHP_EOL . PHP_EOL);
+    print('Code: ' . $exception->getCode() . PHP_EOL . PHP_EOL);
+    print('File: ' . $exception->getFile() . PHP_EOL . PHP_EOL);
+    print('Line: ' . $exception-> getLine() . PHP_EOL . PHP_EOL);
+    print('Trace as string: ' . $exception->getTraceAsString() . PHP_EOL . PHP_EOL);
+    print("Trace:\n");
+    print_r($exception->getTrace());
+    print(PHP_EOL);
+    print('Previous: ' . $exception->getPrevious() . PHP_EOL . PHP_EOL);
+}
