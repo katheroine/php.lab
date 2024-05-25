89e70ee88b3592b42bf0846fe6aeba7356e33e6e
katheroine <katheroine@gmail.com>
2024-05-25 12:34:28 +0200

    Exceptions: Division by zero error [#library]

---
+++ library/classes/exceptions/division_by_zero_error.php
@@ -0,0 +1,19 @@
+<?php
+
+// "DivisionByZeroError is thrown when an attempt is made to divide a number by zero."
+// -- https://www.php.net/manual/en/class.divisionbyzeroerror.php
+
+try {
+    1 / 0;
+} catch (ArithmeticError $exception) {
+    print('As string: ' . $exception . PHP_EOL . PHP_EOL);
+    print('Message: ' . $exception->getMessage() . PHP_EOL . PHP_EOL);
+    print('Code: ' . $exception->getCode() . PHP_EOL . PHP_EOL);
+    print('File: ' . $exception->getFile() . PHP_EOL . PHP_EOL);
+    print('Line: ' . $exception-> getLine() . PHP_EOL . PHP_EOL);
+    print('Trace as string: ' . $exception->getTraceAsString() . PHP_EOL . PHP_EOL);
+    print("Trace:\n");
+    print_r($exception->getTrace());
+    print(PHP_EOL);
+    print('Previous: ' . $exception->getPrevious() . PHP_EOL . PHP_EOL);
+}
