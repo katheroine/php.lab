26381d4c33e71a38f2058b2ef3d84ba3ebd67ee7
katheroine <katheroine@gmail.com>
2024-05-25 02:14:52 +0200

    Exceptions: Argument count error [#library]

---
+++ library/classes/exceptions/argument_count_error.php
@@ -0,0 +1,25 @@
+<?php
+
+// "ArgumentCountError is thrown when too few arguments are passed to a user-defined function or method.
+// This error is also thrown when too many arguments are passed to a non-variadic built-in function."
+// -- https://www.php.net/manual/en/class.argumentcounterror.php
+
+function someFunction(string $someArgument): void
+{
+    print("Some function with argument {$someArgument}\n");
+}
+
+try {
+    someFunction();
+} catch (ArgumentCountError $exception) {
+    print('As string: ' . $exception . PHP_EOL . PHP_EOL);
+    print('Message: ' . $exception->getMessage() . PHP_EOL . PHP_EOL);
+    print('Code: ' . $exception->getCode() . PHP_EOL . PHP_EOL);
+    print('File: ' . $exception->getFile() . PHP_EOL . PHP_EOL);
+    print('Line: ' . $exception-> getLine() . PHP_EOL . PHP_EOL);
+    print('Trace as string: ' . $exception->getTraceAsString() . PHP_EOL . PHP_EOL);
+    print("Trace:\n");
+    print_r($exception->getTrace());
+    print(PHP_EOL);
+    print('Previous: ' . $exception->getPrevious() . PHP_EOL . PHP_EOL);
+}
