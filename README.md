def1c29f2b479aa25c9b1b47f53013e7940258dc
katheroine <katheroine@gmail.com>
2024-05-25 16:48:12 +0200

    Exceptions: Assertion error [#library]

--- README.md
+++ README.md
@@ -133,6 +133,7 @@ Laboratory of PHP.
             1. [Arithmetic error](./library/classes/exceptions/arithmetic_error.php)
             1. [Division by zero error](./library/classes/exceptions/division_by_zero_error.php)
             1. [Unhandled match error](./library/classes/exceptions/unhandled_match_error.php)
+            1. [Assertion error](./library/classes/exceptions/assertion_error.php)
             1. [Closed generator exception](./library/classes/exceptions/closed_generator_exception.php)
             1. [Unexpected value exception (SPL)](./library/classes/exceptions/spl_unexpected_value_exception.php)
             2. [Bad function call exception (SPL)](./library/classes/exceptions/spl_bad_function_call_exception.php)
---
+++ library/classes/exceptions/assertion_error.php
@@ -0,0 +1,19 @@
+<?php
+
+// "AssertionError is thrown when an assertion made via assert() fails."
+// -- https://www.php.net/manual/en/class.assertionerror.php
+
+try {
+    assert(false);
+} catch (AssertionError $exception) {
+    print('As string: ' . $exception . PHP_EOL . PHP_EOL);
+    print('Message: ' . $exception->getMessage() . PHP_EOL . PHP_EOL);
+    print('Code: ' . $exception->getCode() . PHP_EOL . PHP_EOL);
+    print('File: ' . $exception->getFile() . PHP_EOL . PHP_EOL);
+    print('Line: ' . $exception-> getLine() . PHP_EOL . PHP_EOL);
+    print('Trace as string: ' . $exception->getTraceAsString() . PHP_EOL . PHP_EOL);
+    print("Trace:\n");
+    print_r($exception->getTrace());
+    print(PHP_EOL);
+    print('Previous: ' . $exception->getPrevious() . PHP_EOL . PHP_EOL);
+}
