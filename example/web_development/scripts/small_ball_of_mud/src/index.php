85e5e109f4d60543e79006d512a2b5984b8ac1ab
katheroine <katheroine@gmail.com>
2025-11-04 19:51:01 +0100

    Script-level Small Ball of Mud example: Styled with Bootstrap [#web_development]

--- example/web_development/scripts/small_ball_of_mud/src/index.php
+++ example/web_development/scripts/small_ball_of_mud/src/index.php
@@ -2,44 +2,52 @@
 <html lang="en-GB">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
     <meta name="language" content="english">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Small Ball of Mud Example</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <style>
-        *
+        header
         {
-            margin: 0;
-            padding: 0;
+            padding: 2rem;
+        }
+
+        header > h1,
+        header > h2,
+        header > h3
+        {
+            display: inline-block;
+        }
+
+        header > h1,
+        header > h2
+        {
+            margin-right: 2rem;
         }
 
         #intro
         {
+            padding: 4rem 2rem;
             background-color: #14343D;
-            border-radius: 1vw;
-            margin: 2vw;
-            padding: 0.5vw 2vw 1vw 2vw;
             color: #eee;
-        }
-
-        h1
-        {
-            margin: 1.6vw 0;
-            font-size: 2vw;
-        }
-
-        p
-        {
-            margin: 1vw 0;
-            font-size: 1.3vw;
         }
     </style>
 </head>
 
 <body>
+    <header class="container">
+        <h1>PHP.lab</h1>
+        <h2>WebDev Example</h2>
+        <h3>Small Ball of Mud</h3>
+    </header>
     <main>
-        <section id="intro">
-            <h1>Hello, world!</h1>
-            <p>This is Docker example application in PHP <?php echo phpversion(); ?> on Apache.</p>
-        </section>
+        <div id="intro" class="jumbotron">
+            <section id="introduction" class="container">
+                <h1 class="display-3">Hello, world!</h1>
+                <p>This is web page example in <strong>PHP <?php echo phpversion(); ?></strong> on <b>Apache</b>.</p>
+            </section>
+        </div>
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
