71680bf6915ada379bc4d19a4ce443b69b39b4fb
katheroine <katheroine@gmail.com>
2025-11-10 15:33:17 +0100

    Basics: HTTP and HTML forms: Refactoring (Outputs) [#web_development]

--- example/web_development/basics/http/http_and_html_forms/src/outputs.php
+++ example/web_development/basics/http/http_and_html_forms/src/outputs.php
@@ -15,14 +15,23 @@ function buildParamsContent(array $data): string
     $content = '';
 
     if (! empty($data)) {
-        $content .= '<ul>';
-
-        foreach($data as $label => $value) {
-            $content .= '<li><b>' . $label . '</b>: ' . $value . '</li>';
-        }
-
-        $content .= '</ul>';
+        $paramItemsContent = buildParamItemsContent($data);
+        $content .= sprintf('<ul>%s</ul>', $paramItemsContent);
     }
 
     return $content;
+}
+
+function buildParamItemsContent(array $paramItems): string
+{
+    $paramItemsContent = '';
+
+    foreach($paramItems as $label => $value) {
+        $paramItemsContent .= sprintf('<li><b>%s</b>: %s</li>',
+            $label,
+            $value
+        );
+    }
+
+    return $paramItemsContent;
 }
