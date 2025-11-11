20eedca8e34ccc6f5c25744714ba6785ed57e67e
katheroine <katheroine@gmail.com>
2025-11-11 18:25:57 +0100

    Basics: HTTP and Ajax: Refactoring (JS) [#web_development]

--- example/web_development/basics/http/http_and_ajax/src/http_methods.js
+++ example/web_development/basics/http/http_and_ajax/src/http_methods.js
@@ -1,51 +1,33 @@
 function triggerGet() {
-    $.ajax({
-        url: 'http://localhost:8080/server.php',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            $("#board").html("GET triggered");;
-            $("#content").html(data.method);
-        }
-    });
-    console.log("GET triggered");
+    triggerRequest('GET');
 }
 
 function triggerPost() {
-    $.ajax({
-        url: "http://localhost:8080/server.php",
-        type: 'POST',
-        dataType: 'json',
-        success: function(data) {
-            $("#board").html("POST triggered");
-            $("#content").html(data.method);
-        }
-    });
-    console.log("POST triggered");
+    triggerRequest('POST');
 }
 
 function triggerPut() {
-    $.ajax({
-        url: 'http://localhost:8080/server.php',
-        type: 'PUT',
-        dataType: 'json',
-        success: function(data) {
-            $("#board").html("PUT triggered");
-            $("#content").html(data.method);
-        }
-    });
-    console.log("PUT triggered");
+    triggerRequest('PUT');
 }
 
 function triggerDelete() {
+    triggerRequest('DELETE');
+}
+
+const serverUrl = "http://localhost:8080/server.php";
+
+function triggerRequest(method) {
+    const responseDataType = 'json';
+    const message = method + " triggered";
+
     $.ajax({
-        url: "http://localhost:8080/server.php",
-        type: 'DELETE',
-        dataType: 'json',
+        url: serverUrl,
+        type: method,
+        dataType: responseDataType,
         success: function(data) {
-            $("#board").html("DELETE triggered");;
+            $("#board").html(message);
             $("#content").html(data.method);
         }
     });
-    console.log("DELETE triggered");
+    console.log(message);
 }
