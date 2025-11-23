13a5048eb4a53e38b465fc16d10bb68ef1881f26
katheroine <katheroine@gmail.com>
2025-11-23 02:21:47 +0100

    Basics: HTTP with pure PHP: Refactoring [#web_development]

--- example/web_development/basics/http/http_with_pure_php/src/communication.php
+++ example/web_development/basics/http/http_with_pure_php/src/communication.php
@@ -1,100 +1,71 @@
 <?php
 
+const METHOD_DATA_KEY = 'method';
+const TEXT_DATA_KEY = 'some_text';
+const NUMBER_DATA_KEY = 'some_number';
+
 function communicate(): array
 {
-    $request = [];
-    $response= [];
+    $requestMethod = null;
+    $request = null;
+    $response = null;
 
-    if (isset($_REQUEST['method'])) {
-        $requestMethod = $_REQUEST['method'];
-
-        $request = [
-            'some_text' => $_REQUEST['some_text'],
-            'some_number' => $_REQUEST['some_number'],
-        ];
-
-        $url = 'http://localhost/server.php';
-
-        switch($requestMethod) {
-            case 'GET':
-                $query = http_build_query($request);
-                $queryUrl = $url . '?' . $query;
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $queryUrl);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $response = json_decode(
-                    curl_exec($ch)
-                );
-                curl_close($ch);
-                break;
-            case 'POST':
-                $query = http_build_query($request);
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $url);
-                curl_setopt($ch, CURLOPT_POST, true);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $response = json_decode(
-                    curl_exec($ch)
-                );
-                curl_close($ch);
-                break;
-            case 'PUT':
-                $query = http_build_query($request);
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $url);
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                    'Content-Type: application/x-www-form-urlencoded',
-                    'Content-Length: ' . strlen($query),
-                    'My-Very-Own-Client-Data: hello',
-                ]);
-                $response = json_decode(
-                    curl_exec($ch)
-                );
-                curl_close($ch);
-                break;
-            case 'PATCH':
-                $headers = [
-                    'X-HTTP-Method-Override: PATCH',
-                ];
-                $query = http_build_query($request);
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $url);
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $response = json_decode(
-                    curl_exec($ch)
-                );
-                curl_close($ch);
-                break;
-            case 'DELETE':
-                $headers = [
-                    'X-HTTP-Method-Override: DELETE',
-                ];
-                $query = http_build_query($request);
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, $url);
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $response = json_decode(
-                    curl_exec($ch)
-                );
-                curl_close($ch);
-                break;
-        }
-
-        // var_dump(curl_errno($ch));
-        // if(curl_error($ch)) {
-        //     var_dump(curl_error($ch));
-        // }
+    if (! isset($_REQUEST[METHOD_DATA_KEY])) {
+        return [];
     }
+
+    $requestMethod = $_REQUEST[METHOD_DATA_KEY];
+
+    $request = [
+        TEXT_DATA_KEY => $_REQUEST[TEXT_DATA_KEY],
+        NUMBER_DATA_KEY => $_REQUEST[NUMBER_DATA_KEY],
+    ];
+
+    $url = 'http://localhost/server.php';
+
+    $query = http_build_query($request);
+    $ch = curl_init();
+
+    switch($requestMethod) {
+        case 'GET':
+            $url = $url . '?' . $query;
+            break;
+        case 'POST':
+            curl_setopt($ch, CURLOPT_POST, true);
+            break;
+        case 'PUT':
+            $headers = [
+                'Content-Type: application/x-www-form-urlencoded',
+                'Content-Length: ' . strlen($query),
+            ];
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $requestMethod);
+            break;
+        case 'PATCH':
+        case 'DELETE':
+            $headers = [
+                'X-HTTP-Method-Override: ' . $requestMethod,
+            ];
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $requestMethod);
+            break;
+    }
+
+    $headers[] = 'My-Very-Own-Client-Data: hello';
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $response = json_decode(
+        curl_exec($ch)
+    );
+
+    curl_close($ch);
+
+    // var_dump(curl_errno($ch));
+    // if(curl_error($ch)) {
+    //     var_dump(curl_error($ch));
+    // }
 
     return [
         $requestMethod,
